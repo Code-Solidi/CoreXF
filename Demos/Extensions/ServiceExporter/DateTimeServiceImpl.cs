09ce625f3a40a3b2a05bddde95307e37fe00700d
Alexander Christov <christov.alex@gmail.com>
2020-02-03 17:18:47 +0200

    Service created and accessible thru built-in DI in host application.

---
+++ Demos/Extensions/ServiceExporter/DateTimeServiceImpl.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+using DateTimeService;
+
+namespace ServiceExporter
+{
+    internal class DateTimeServiceImpl : IDateTimeService
+    {
+        public DateTime Get() => DateTime.UtcNow;
+    }
+}
---
+++ Demos/Extensions/ServiceExporter/Startup.cs
@@ -0,0 +1,23 @@
+﻿using DateTimeService;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ServiceExporter
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IDateTimeService, DateTimeServiceImpl>();
+        }
+    }
+}
